a86c1f3515412f5064c2588aa230d47de3b1728a
Inshad <minshad010@gmail.com>
2026-06-17 16:20:18 +0530

    fix: resolve react-refresh lint warnings in UI components properly

--- eslint.config.js
+++ eslint.config.js
@@ -32,7 +32,7 @@ export default tseslint.config(
           ],
         },
       ],
-      "react-refresh/only-export-components": "off",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
